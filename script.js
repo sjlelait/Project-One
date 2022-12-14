6a9dc68c2e7bbf8398c21b4ace061f18151047f9
Sara Le Lait <sjlelait@gmail.com>
2022-12-14 16:26:35 -0700

    wip: adds starting variables

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // API source code 
 //"https://www.dictionaryapi.com/api/v3/references/thesaurus/json/"+userInput+"?=020263dd-5ac1-411c-864b-fcf1f0644c9b"
+
+const $word = $(`#word`);
+const $response = $(`#syn`);
+const $shortDef = $(`#short-def`);
+const $input = $(`input[type="text]`);
+
+let wordData, userInput
+
+
