f04ca8c121a6675ee9627a3d21271f10b78afe69
Sara Le Lait <sjlelait@gmail.com>
2022-12-14 19:29:28 -0700

    wip: adds css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // API source code 
 //"https://www.dictionaryapi.com/api/v3/references/thesaurus/json/"+userInput+"?key=020263dd-5ac1-411c-864b-fcf1f0644c9b"
 // KEY 020263dd-5ac1-411c-864b-fcf1f0644c9b
+
+
 const $word = $(`#word`);
 const $synonym = $(`#syn`);
 const $shortDef = $(`#short-def`);
@@ -28,10 +30,8 @@ $(document).ready(function() {
 
     function render() {
         $word.text(wordData[0].hwi.hw)
-        $synonym.text(wordData[0].meta.syns[0,1,2])
-        $shortDef.text(wordData[0].shortdef)
+        $synonym.text(wordData[0].meta.syns[0,1].join(",  "))
+        $shortDef.text(wordData[0].shortdef.join(",  "))
+    };
 
-        console.log(wordData[0])
-    }
-
-})
+});
